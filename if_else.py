744c95c4550f1a078f41d1ac720db4979de3f34f
whoismaruf <whoismaruf@outlook.com>
2021-03-11 12:09:11 +0600

    If-else challenge solved!

---
+++ if_else.py
@@ -0,0 +1,17 @@
+def checking(n):
+	if n%2 == 0:
+		if (n in range(2,5)) or n>20:
+			if n in range(6,20):
+				print("weird")
+			else:
+				print("Not weird")
+		else:
+			print("weird")
+	else:
+		if n%2 != 0:
+			print("weird")
+		else:
+			print("Invalid")
+
+
+checking(26)
